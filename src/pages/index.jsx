265ccf55481208234ae68b52bed5b09e72e7c3c4
juandavidGF <davad701@gmail.com>
2023-03-16 16:28:47 -0500

    last of not last

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -60,10 +60,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
 				<div className={styles.content}>
-				<p>artmelon.me</p>
-					<div>
-						<Link href="https://artmelon.me"><img src="/icon.png" alt="logo artMelon" width="100px" height="100px"  /></Link>
-					</div>
+					<Link href="https://artmelon.me"><img src="/icon.png" alt="logo artMelon" width="100px" height="100px"  /></Link>
 					<h1>{t('title')}</h1>
 					<h2>{t('subtitle')}</h2>
 					<p className={styles.date}>{t('p1')}</p>
